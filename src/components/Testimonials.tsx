e6203de925f7635b608bf6e3f66217ed5181bcd5
DevGhost511 <rosemax994@gmail.com>
2025-05-29 17:06:41 +0900

    Add : Testimonial section added

---
+++ src/components/Testimonials.tsx
@@ -0,0 +1,13 @@
+
+import { Star } from 'lucide-react';
+
+interface TestimonialProps {
+  quote: string;
+  author: string;
+  role: string;
+  rating: number;
+}
+
+
+
+export default Testimonials;
